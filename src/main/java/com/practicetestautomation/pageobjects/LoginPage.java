24382e4c27a45291a8b65adacd3fe3ca92e9a0a9
marican <muhammet.arican@xl2.de>
2024-12-27 23:55:45 +0100

    refactoring

--- src/main/java/com/practicetestautomation/pageobjects/LoginPage.java
+++ src/main/java/com/practicetestautomation/pageobjects/LoginPage.java
@@ -3,22 +3,15 @@ package com.practicetestautomation.pageobjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-
-public class LoginPage {
-    private WebDriver driver;
-    private WebDriverWait wait;
+public class LoginPage extends BasePage {
     private By usernameInputLocator = By.id("username");
     private By passwordInputLocator = By.id("password");
     private By submitButtonLocator = By.id("submit");
     private By errorMessageLocator = By.id("error");
 
     public LoginPage (WebDriver driver){
-        this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        super(driver);
     }
 
     public void enterUsername(String username){
@@ -33,14 +26,15 @@ public class LoginPage {
         driver.findElement(submitButtonLocator).click();
     }
 
-    public void executeLogin(String username, String password){
+    public SuccessfulLoginPage executeLogin(String username, String password){
         enterUsername(username);
         enterPassword(password);
         clickSubmitButton();
+        return new SuccessfulLoginPage(driver);
     }
 
     public String getErrorMessage(){
-        WebElement errorMessageElement = wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessageLocator));
+        WebElement errorMessageElement = waitForElement(errorMessageLocator);
         return errorMessageElement.getText();
     }
 }
